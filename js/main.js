cdd8df431ede43f557ea83e7b5f4a1e01c851f3d
ELVIS NASCIMENTO <elvisnascimentodev@gmail.com>
2023-07-27 10:37:18 -0300

    projeto bem encaminhado mas ainda faltam algumas coisas

---
+++ js/main.js
@@ -0,0 +1,61 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    // Faz a chamada à API para autenticar o usuário
+    authenticateUser(username, password)
+      .then(userData => {
+        // Exibe os dados do usuário autenticado no crachá
+        showUserData(userData);
+      })
+      .catch(error => {
+        // Caso haja erro na autenticação, exibe mensagem de erro
+        alert(error);
+      });
+  });
+  
+  async function authenticateUser(username, password) {
+    // Faz a requisição para a API de autenticação com os dados do usuário
+    const response = await fetch("https://dummyjson.com/users");
+    const data = await response.json();
+    console.log(data.users);
+  
+      const users = data.users;
+    
+      // Verifica se os dados do usuário estão corretos
+      const authenticatedUser = users.
+      find(user => user.username === username &&
+         user.password === password);
+    
+      if (!authenticatedUser) {
+        throw new Error("Usuário ou senha inválidos.");
+      }
+    
+      return authenticatedUser;
+  }
+  
+  function showUserData(userData) {
+    // Esconde o formulário de login
+    document.getElementById("loginForm").style.display = "none";
+  
+    // Preenche os dados do crachá com as informações do usuário autenticado
+    document.getElementById("cracha").style.display = "block";
+    document.getElementById("cardNumber").innerText = userData.cardNumber;
+    document.getElementById("userImage").src = userData.image;
+    document.getElementById("firstName").innerText = userData.firstName;
+    document.getElementById("lastName").innerText = userData.lastName;
+    document.getElementById("address").innerText = userData.address;
+    document.getElementById("city").innerText = userData.city;
+    document.getElementById("email").innerText = userData.email;
+    document.getElementById("phone").innerText = userData.phone;
+    document.getElementById("gender").innerText = userData.gender;
+    document.getElementById("hair").innerText = userData.hair;
+    document.getElementById("eye").innerText = userData.eyeColor;
+    document.getElementById("eyeColor").innerText = userData.eyeColor;
+    document.getElementById("height").innerText = userData.height;
+    document.getElementById("weight").innerText = userData.weight;
+
+  }
+  
